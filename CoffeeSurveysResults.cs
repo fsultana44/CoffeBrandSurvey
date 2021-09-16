4f2cbe34eeb9e226f9a728f4eea5822a16300e7c
Fatema Sultana <fatema.chowdury@hiottu.fi>
2021-09-16 15:17:12 +0300

    Add project files.

---
+++ CoffeeSurveysResults.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeBrandSurvey
+{
+    class CoffeeSurveysResults
+    {
+        static public class QAResults
+        {   /// <Score determine by number.summary>
+        /// 
+        /// </summary>
+            public static double ServiceScore { get; set; } = 8.0;
+            public static double CoffeeTypesScore { get; set; } = 8.5;
+            public static double PriceScore { get; set; } = 6.0;
+            public static double FoodScore { get; set; } = 8.0;
+            public static double WouldRecommendScore { get; set; } = 8.0;
+            public static string FavouriteProduct { get; set; } = Cappucino;
+            public static string LeastFavouriteProduct { get; set; } = Granola;
+
+            //Survey count
+            public static float SurveyNumber { get; set; } = 500;
+            public static float RespondedNumber { get; set; } = 300;
+            public static float RewardNumber { get; set; } = 100;
+            public static string Granola { get; private set; }
+            public static string Cappucino { get; private set; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static CoffeBrandSurvey.CoffeeSurveysResults;
+
+namespace CoffeBrandSurvey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double respondRate;
+            double unanswereSurvey;
+            double overallScore;
+            // calculate the result
+            respondRate = QAResults.SurveyNumber / QAResults.RespondedNumber;
+            unanswereSurvey = QAResults.SurveyNumber - QAResults.RespondedNumber;
+
+            // calculaate average score for all main categories.
+            overallScore = (QAResults.ServiceScore + QAResults.CoffeeTypesScore + QAResults.PriceScore + QAResults.FoodScore) /4;
+            Console.WriteLine($"Respons rate: {respondRate}");
+            Console.WriteLine($"Unanswered Survey: {unanswereSurvey}");
+            Console.WriteLine($"Overall score for all categories: {overallScore}");
+
+            /// do the more analysis to get the answer of the following questions
+            /// For What  the score is higher (coffee/food)
+            /// Score of customers recommendation.
+            /// and the least and most popular products.
+            /// Do it by bool expression,
+            bool higherCoffeeScore = QAResults.CoffeeTypesScore > QAResults.FoodScore;
+            bool customerRecommendation = QAResults.WouldRecommendScore >= 8;
+            bool leastGranolaMostCappucino =QAResults.LeastFavouriteProduct == "Granola" && QAResults.FavouriteProduct == "Cappucino";
+            Console.WriteLine($"Higher Coffee Score is: {higherCoffeeScore}");
+            Console.WriteLine($"Customers recommendation : {customerRecommendation}");
+            Console.WriteLine($"Least favourite Granola and favourite Cappucino : {leastGranolaMostCappucino}");
+
+
+        }
+    }
+}
